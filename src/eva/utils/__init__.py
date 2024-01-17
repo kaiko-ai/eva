6ad3a9078007913fd1dd8b12c3090ec8815bb598
ioangatop <johngatop@gmail.com>
2024-01-17 16:44:25 +0100

    add loguru logger

---
+++ src/eva/utils/__init__.py
@@ -0,0 +1,4 @@
+"""Utilities and helper functionalities."""
+from eva.utils.logger import logger
+
+__all__ = ["logger"]
---
+++ src/eva/utils/logger.py
@@ -0,0 +1,26 @@
+"""Initializes the logger of the library.
+
+This customizable logger can be used by just importing
+`loguru` from everywhere as follows:
+>>> from loguru import logger
+>>> logger.info(...)
+"""
+import sys
+
+from loguru import logger
+
+
+def _initialize_logger() -> None:
+    """Manipulates and customizes the logger."""
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        format="<magenta>[{time:HH:mm:ss}]</magenta>"
+        " <bold><level>{level: <15}</level></bold> "
+        " | {message}",
+        colorize=True,
+        level="INFO",
+    )
+
+
+_initialize_logger()
