13cb9612664b8f160f9d4bb0ee6088ffb472655d
ioangatop <johngatop@gmail.com>
2024-01-24 13:23:03 +0100

    update public api

--- src/eva/__init__.py
+++ src/eva/__init__.py
@@ -1,14 +1,9 @@
 """EVA core API."""
-from eva import data, metrics, models, utils
 from eva.data.datamodules import DataloadersSchema, DataModule, DatasetsSchema
 from eva.models.modules import HeadModule
 from eva.trainers import Trainer
 
 __all__ = [
-    "data",
-    "metrics",
-    "models",
-    "utils",
     "DataloadersSchema",
     "DataModule",
     "DatasetsSchema",
