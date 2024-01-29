9bb997aed19efb198c7a8a15bfaeb9e8e456b7c9
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-01-29 15:32:59 +0100

    first working version

--- src/eva/vision/data/datasets/bach.py
+++ src/eva/vision/data/datasets/bach.py
@@ -1,14 +1,15 @@
 """Bach dataset class."""
+
+import dataclasses
 import os
 from pathlib import Path
-from typing import Dict, Tuple ,List
+from typing import Dict, List, Tuple
 
 import numpy as np
 import pandas as pd
 from loguru import logger
-from torchvision.datasets.utils import download_and_extract_archive, download_url
+from torchvision.datasets.utils import download_and_extract_archive
 from typing_extensions import override
-import dataclasses
 
 from eva.vision.data.datasets.vision import VisionDataset
 from eva.vision.file_io import image_io
@@ -16,6 +17,8 @@ from eva.vision.file_io import image_io
 
 @dataclasses.dataclass
 class DownloadResource:
+    """Contains download information for a specific resource."""
+
     filename: str
     url: str
     md5: str | None = None
@@ -30,14 +33,20 @@ class BachDataset(VisionDataset[np.ndarray]):
         "split": "split",
     }
 
-    classes: str = [
+    classes: List[str] = [
         "Normal",
         "Benign",
         "InSitu",
         "Invasive",
     ]
 
-    resources: List[DownloadResource] = DownloadResource(filename="ICIAR2018_BACH_Challenge.zip", url="https://zenodo.org/records/3632035/files/ICIAR2018_BACH_Challenge.zip", md5="8ae1801334aa943c44627c1eef3631b2")
+    resources: List[DownloadResource] = [
+        DownloadResource(
+            filename="ICIAR2018_BACH_Challenge.zip",
+            url="https://zenodo.org/records/3632035/files/ICIAR2018_BACH_Challenge.zip",
+            md5="8ae1801334aa943c44627c1eef3631b2",
+        ),
+    ]
 
     def __init__(
         self,
@@ -65,72 +74,92 @@ class BachDataset(VisionDataset[np.ndarray]):
 
         self._data: pd.DataFrame
 
+        self._path_column = self._column_mapping["path"]
+        self._split_column = self._column_mapping["split"]
+        self._target_column = self._column_mapping["target"]
+
     @override
     def __len__(self) -> int:
         return len(self._data)
 
     @override
     def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray]:
-        image = image_io.load_image(self._data.at[index, self._column_mapping["path"]])
-        target = self._data.at[index, self._column_mapping["target"]]
-        raise image, target
+        image = image_io.load_image(self._get_image_path(index))
+        target = self._data.at[index, self._target_column]
+        return image, target
 
     @override
-    def _prepare_data(self) -> None:
+    def prepare_data(self) -> None:
         if not self._exists():
             self._download()
-
-        self._create_manifest()
+            self._create_manifest()
 
     @override
-    def _setup(self) -> None:
+    def setup(self) -> None:
         self._data = self._load_manifest()
 
         if self._split:
-            self._data = self._data[self._data[self._column_mapping["split"]] == self._split]
+            split_filter = self._data[self._split_column] == self._split
+            self._data = self._data.loc[split_filter].reset_index(drop=True)
 
     @property
-    def class_to_idx(self) -> Dict[str, int]:
+    def _class_to_idx(self) -> Dict[str, int]:
         return {_class: i for i, _class in enumerate(self.classes)}
 
     def _exists(self) -> bool:
-        return all(
-            os.path.join(self._root_dir, p) for p in self._data[self._column_mapping["path"]]
+        return os.path.exists(self._manifest_path) and os.path.isdir(
+            os.path.join(self._root_dir, "ICIAR2018_BACH_Challenge")
         )
 
     def _download(self) -> None:
         os.makedirs(self._root_dir, exist_ok=True)
 
         for r in self.resources:
-            download_and_extract_archive(r.url, download_root=self._root_dir, filename=r.filename, md5=r.md5)
+            download_and_extract_archive(
+                r.url, download_root=self._root_dir, filename=r.filename, md5=r.md5
+            )
+
+    def _get_image_path(self, index: int) -> str:
+        return os.path.join(self._root_dir, self._data.at[index, self._path_column])
 
     def _load_manifest(self) -> pd.DataFrame:
         logger.info(f"Load manifest from {self._manifest_path}")
         return pd.read_parquet(self._manifest_path)
 
     def _create_manifest(self) -> pd.DataFrame:
-        # load image paths & labels
-        df_manifest = pd.DataFrame(Path(self._root_dir).glob("**/*.tif"), columns=["path"])
-        df_manifest["label"] = df_manifest["path"].apply(lambda p: Path(p).parent.name)
+        # load image paths & targets
+        paths = Path(self._root_dir).glob("**/*.tif")
+        df_manifest = pd.DataFrame(paths, columns=[self._path_column])  # type: ignore
+        df_manifest[self._target_column] = df_manifest[self._path_column].apply(
+            lambda p: Path(p).parent.name
+        )
 
-        if not df_manifest["label"].isin(self.classes).all():
-            raise ValueError(f"Unexpected classes: {df_manifest['label'].unique()}")
+        if not all(df_manifest[self._target_column].isin(self.classes)):
+            raise ValueError(f"Unexpected classes: {df_manifest[self._target_column].unique()}")
+
+        df_manifest[self._target_column].replace(self._class_to_idx, inplace=True)
+        df_manifest[self._path_column] = df_manifest[self._path_column].apply(
+            lambda x: Path(x).relative_to(self._root_dir).as_posix()
+        )
 
         # create splits
         # TODO: refactor this into a shared split module: https://github.com/kaiko-ai/eva/issues/75
         train_fraction, val_fraction = 0.7, 0.15
-        df_manifest["split"] = ""
+        df_manifest[self._split_column] = ""
         dfs = []
-        for label in df_manifest["label"].unique():
-            label_filter = df_manifest["label"] == label
-            df = df_manifest[label_filter].sort_values(by="path").reset_index(drop=True)
-            n_train, n_val = round(df.shape[0] * train_fraction), round(df.shape[0] * val_fraction)
-            df.loc[:n_train, "split"] = "train"
-            df.loc[n_train : n_train + n_val, "split"] = "val"
-            df.loc[n_train + n_val :, "split"] = "test"
-            dfs.append(df)
+        for _, df_target in df_manifest.groupby(self._target_column):
+            df_target = df_target.sort_values(by=self._path_column).reset_index(drop=True)
+            n_train, n_val = round(df_target.shape[0] * train_fraction), round(
+                df_target.shape[0] * val_fraction
+            )
+            df_target.loc[:n_train, self._split_column] = "train"
+            df_target.loc[n_train : n_train + n_val, self._split_column] = "val"
+            df_target.loc[n_train + n_val :, self._split_column] = "test"
+            dfs.append(df_target)
 
         # save manifest
-        df_manifest = pd.concat(dfs).sort_values(by=["split", "label"]).reset_index(drop=True)
+        df_manifest = pd.concat(dfs).reset_index(drop=True)
         df_manifest.to_parquet(self._manifest_path)
         logger.info(f"Saved manifest to {self._manifest_path}")
+
+        return df_manifest
