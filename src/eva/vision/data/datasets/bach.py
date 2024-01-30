4ac3591a0e805dc860d5a1493b791607f0221de1
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-01-30 16:02:26 +0100

    added split_ratios argument

--- src/eva/vision/data/datasets/bach.py
+++ src/eva/vision/data/datasets/bach.py
@@ -8,7 +8,7 @@ from typing import Dict, List, Literal, Tuple
 import numpy as np
 import pandas as pd
 from loguru import logger
-from torchvision.datasets.utils import download_and_extract_archive
+from torchvision.datasets.utils import download_url, extract_archive
 from typing_extensions import override
 
 from eva.vision.data.datasets.vision import VisionDataset
@@ -22,6 +22,20 @@ class DownloadResource:
     filename: str
     url: str
     md5: str | None = None
+
+
+@dataclasses.dataclass
+class SplitRatios:
+    """Contains split ratios for train, val and test."""
+
+    train: float = 0.6
+    val: float = 0.1
+    test: float = 0.3
+
+    def __post_init__(self):
+        """Verify that the split ratios sum to 1.0."""
+        if self.train + self.val + self.test != 1.0:
+            raise ValueError("Split ratios must sum to 1.0")
 
 
 class BachDataset(VisionDataset[np.ndarray]):
@@ -53,7 +67,8 @@ class BachDataset(VisionDataset[np.ndarray]):
         root_dir: str,
         manifest_path: str,
         split: Literal["train", "val", "test"] | None,
-        download: bool = False,
+        split_ratios: SplitRatios | None = None,
+        download: bool = True,
         column_mapping: Dict[str, str] = default_column_mapping,
     ):
         """Initialize dataset.
@@ -63,6 +78,7 @@ class BachDataset(VisionDataset[np.ndarray]):
                 and extracted here, if it does not already exist.
             manifest_path: Path to the dataset manifest file.
             split: Dataset split to use. If None, the entire dataset is used.
+            split_ratios: Ratios for the train, val and test splits.
             download: Whether to download the data for the specified split.
                 Note that the download will be executed only by additionally
                 calling the :meth:`prepare_data` method and if the data does not exist yet on disk.
@@ -83,6 +99,9 @@ class BachDataset(VisionDataset[np.ndarray]):
         self._split_column = self._column_mapping["split"]
         self._target_column = self._column_mapping["target"]
 
+        if split_ratios is None:
+            self._split_ratios = SplitRatios(train=0.7, val=0.15, test=0.15)
+
     @override
     def __len__(self) -> int:
         return len(self._data)
@@ -97,6 +116,12 @@ class BachDataset(VisionDataset[np.ndarray]):
     def prepare_data(self) -> None:
         if self._download:
             self._download_dataset()
+        if not os.path.isdir(os.path.join(self._root_dir, "ICIAR2018_BACH_Challenge")):
+            logger.info("Extracting archive ...")
+            extract_archive(
+                from_path=os.path.join(self._root_dir, "ICIAR2018_BACH_Challenge.zip"),
+                to_path=self._root_dir,
+            )
         if not os.path.exists(self._manifest_path):
             self._create_manifest()
 
@@ -116,15 +141,13 @@ class BachDataset(VisionDataset[np.ndarray]):
         os.makedirs(self._root_dir, exist_ok=True)
 
         for r in self.resources:
-            download_and_extract_archive(
-                r.url, download_root=self._root_dir, filename=r.filename, md5=r.md5
-            )
+            download_url(r.url, root=self._root_dir, filename=r.filename, md5=r.md5)
 
     def _get_image_path(self, index: int) -> str:
         return os.path.join(self._root_dir, self._data.at[index, self._path_column])
 
     def _load_manifest(self) -> pd.DataFrame:
-        logger.info(f"Load manifest from {self._manifest_path}")
+        logger.info(f"Load manifest from {self._manifest_path}.")
         df_manifest = pd.read_parquet(self._manifest_path)
         self._verify_manifest(df_manifest)
         return df_manifest
@@ -133,13 +156,12 @@ class BachDataset(VisionDataset[np.ndarray]):
         if len(df_manifest) != 400:
             raise ValueError(f"Expected 400 samples but manifest lists {len(df_manifest)}.")
 
-        if (df_manifest["target"].value_counts() == 100).all():
+        if not (df_manifest["target"].value_counts() == 100).all():
             raise ValueError("Expected 100 samples per class.")
 
     def _create_manifest(self) -> pd.DataFrame:
         # load image paths & targets
-        paths = Path(self._root_dir).glob("**/*.tif")
-        df_manifest = pd.DataFrame(paths, columns=[self._path_column])  # type: ignore
+        df_manifest = pd.DataFrame({self._path_column: Path(self._root_dir).glob("**/*.tif")})
         df_manifest[self._target_column] = df_manifest[self._path_column].apply(
             lambda p: Path(p).parent.name
         )
@@ -147,20 +169,19 @@ class BachDataset(VisionDataset[np.ndarray]):
         if not all(df_manifest[self._target_column].isin(self.classes)):
             raise ValueError(f"Unexpected classes: {df_manifest[self._target_column].unique()}")
 
-        df_manifest[self._target_column].replace(self._class_to_idx, inplace=True)
+        df_manifest[self._target_column] = df_manifest[self._target_column].map(self._class_to_idx)  # type: ignore
         df_manifest[self._path_column] = df_manifest[self._path_column].apply(
             lambda x: Path(x).relative_to(self._root_dir).as_posix()
         )
 
         # create splits
         # TODO: refactor this into a shared split module: https://github.com/kaiko-ai/eva/issues/75
-        train_fraction, val_fraction = 0.7, 0.15
         df_manifest[self._split_column] = ""
         dfs = []
         for _, df_target in df_manifest.groupby(self._target_column):
             df_target = df_target.sort_values(by=self._path_column).reset_index(drop=True)
-            n_train, n_val = round(df_target.shape[0] * train_fraction), round(
-                df_target.shape[0] * val_fraction
+            n_train, n_val = round(df_target.shape[0] * self._split_ratios.train), round(
+                df_target.shape[0] * self._split_ratios.val
             )
             df_target.loc[:n_train, self._split_column] = "train"
             df_target.loc[n_train : n_train + n_val, self._split_column] = "val"
