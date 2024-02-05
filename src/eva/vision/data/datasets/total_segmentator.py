f09b4df09d8b1322e77acc95dbaba28c3bf3c1e6
roman807 <roman.moser@gmx.net>
2024-02-05 18:34:34 +0100

    add total_segmentator test data

--- src/eva/vision/data/datasets/total_segmentator.py
+++ src/eva/vision/data/datasets/total_segmentator.py
@@ -30,15 +30,16 @@ class DownloadResource:
 class SplitRatios:
     """Contains split ratios for train, val and test."""
 
-    train: float = 0.33
-    val: float = 0.33
-    test: float = 0.34
+    train: float = 0.6
+    val: float = 0.2
+    test: float = 0.2
 
 
 class TotalSegmentator(VisionDataset[np.ndarray]):
-    """TotalSegmentator dataset class."""
-
-    sample_every_n_slice: int = 10
+    """TotalSegmentator dataset class.
+    
+    Create the multilabel classification dataset for the TotalSegmentator data.
+    """
 
     resources: List[DownloadResource] = [
         DownloadResource(
@@ -47,7 +48,7 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
             md5="6b5524af4b15e6ba06ef2d700c0c73e0",
         ),
     ]
-    # TODO: to use complete instead of small dataset: set:
+    # TODO: switch to use complete instead of small dataset:
     #  - url="https://zenodo.org/records/10047292/files/Totalsegmentator_dataset_v201.zip"
     #  - md5="fe250e5718e0a3b5df4c4ea9d58a62fe"
 
@@ -56,6 +57,7 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
         root_dir: str,
         split: Literal["train", "val", "test"],
         split_ratios: SplitRatios | None = None,
+        sample_every_n_slice: int = 25,
         download: bool = False,
     ):
         """Initialize dataset.
@@ -65,6 +67,7 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
                 and extracted here, if it does not already exist.
             split: Dataset split to use. If None, the entire dataset is used.
             split_ratios: Ratios for the train, val and test splits.
+            sample_every_n_slice: Number of slices to skip when sampling slices from the 3D images.
             download: Whether to download the data for the specified split.
                 Note that the download will be executed only by additionally
                 calling the :meth:`prepare_data` method and if the data does not exist yet on disk.
@@ -78,7 +81,9 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
         self._data: pd.DataFrame
         self._path_key, self._split_key = "path", "split"
         self._split_ratios = split_ratios or self.default_split_ratios
-        self._classes = self._get_classes()
+        self._sample_every_n_slice = sample_every_n_slice
+        self._manifest_path = os.path.join(self._root_dir, "manifest.csv")
+        self._classes = None
 
     @override
     def __len__(self) -> int:
@@ -99,12 +104,15 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
             logger.info("Extracting archive ...")
             extract_archive(
                 from_path=os.path.join(self._root_dir, "Totalsegmentator_dataset_v201.zip"),
-                to_path=self._root_dir,
+                to_path=os.path.join(self._root_dir, "Totalsegmentator_dataset_v201")
             )
+        self._classes = self._get_classes()
 
     @override
     def setup(self) -> None:
         df = self._load_dataset()
+        if not os.path.isfile(self._manifest_path):
+            self._save_manifest(df)
         df = self._generate_ordered_splits(df)
         self._verify_dataset(df)
 
@@ -112,8 +120,8 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
 
     @property
     def default_split_ratios(self) -> SplitRatios:
-        """Returns the defaults split ratios."""
-        return SplitRatios(train=0.6, val=0.2, test=0.2)
+        """Returns the default split ratios."""
+        return SplitRatios()
 
     def _download_dataset(self) -> None:
         os.makedirs(self._root_dir, exist_ok=True)
@@ -132,7 +140,7 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
             for f in sorted(
                 os.listdir(
                     os.path.join(
-                        self._root_dir, "Totalsegmentator_dataset_small_v201/s0011/segmentations"
+                        self._root_dir, "Totalsegmentator_dataset_v201/s0011/segmentations"
                     )
                 )
             )
@@ -140,9 +148,13 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
         return classes
 
     def _load_dataset(self) -> pd.DataFrame:
+        """Loads the dataset manifest from a CSV file or creates the dataframe it does not exist."""
+
+        if os.path.isfile(self._manifest_path):
+            return pd.read_csv(self._manifest_path)
+        
         data_dict = defaultdict(list)
         for i, path in enumerate(Path(self._root_dir).glob("**/*ct.nii.gz")):
-            # img_data  = nib.load(path).get_fdata()
             img_data = image_io.load_nifti_image(path)
             n_slices = img_data.shape[-1]
 
@@ -150,12 +162,12 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
             masks = {}
             for cl in self._classes:
                 mask_path = os.path.join(path.parents[0], "segmentations", cl + ".nii.gz")
-                masks[cl] = image_io.load_nifti_image(mask_path)  # nib.load(mask).get_fdata()
+                masks[cl] = image_io.load_nifti_image(mask_path)
 
             # sample slices and extract label for each class:
             np.random.seed(i)
-            start_slice = np.random.choice(min(self.sample_every_n_slice, n_slices))
-            for i in range(start_slice, n_slices, self.sample_every_n_slice):
+            start_slice = np.random.choice(min(self._sample_every_n_slice, n_slices))
+            for i in range(start_slice, n_slices, self._sample_every_n_slice):
                 data_dict["path"].append(path)
                 data_dict["slice"].append(i)
                 for cl in self._classes:
@@ -163,11 +175,19 @@ class TotalSegmentator(VisionDataset[np.ndarray]):
                     data_dict[cl].append(label)
 
             df = pd.DataFrame(data_dict)
+            # print(df)
 
         return df
+    
+    def _save_manifest(self, df: pd.DataFrame) -> None:
+        """Saves the dataset manifest to a CSV file."""
+
+        manifest_path = os.path.join(self._root_dir, "manifest.csv")
+        df.to_csv(manifest_path, index=False)
 
     def _generate_ordered_splits(self, df: pd.DataFrame) -> pd.DataFrame:
         """Orders each class by path and then splits it into train, val and test sets."""
+
         paths = sorted(df[self._path_key].unique())
         n_train_paths, n_val_paths = (
             round(len(paths) * self._split_ratios.train),
---
+++ tests/eva/vision/data/datasets/test_total_segmentator.py
@@ -0,0 +1,45 @@
+"""TotalSegmentator dataset tests."""
+
+import os
+from typing import Literal
+from unittest.mock import patch
+
+import numpy as np
+import pytest
+
+from eva.vision.data import datasets
+
+
+@pytest.mark.parametrize(
+    "split",
+    ["train", "val", "test"],
+)
+def test_sample(total_segmentator_dataset: datasets.TotalSegmentator) -> None:
+    """Tests the format of a dataset sample."""
+    
+    # assert data sample is a tuple
+    sample = total_segmentator_dataset[0]
+    assert isinstance(sample, tuple)
+    assert len(sample) == 2
+
+    # assert the format of the `image` and `target`
+    image, target = sample
+    assert isinstance(image, np.ndarray)
+    assert image.shape == (16, 16)
+    assert isinstance(target, np.ndarray)
+    assert all(target == [0, 0, 0])
+
+
+@pytest.fixture(scope="function")
+def total_segmentator_dataset(split: Literal["train", "val", "test"], assets_path: str) -> datasets.Bach:
+    """TotalSegmentator dataset fixture."""
+
+    with patch("eva.vision.data.datasets.TotalSegmentator._verify_dataset") as _:
+        ds = datasets.TotalSegmentator(
+            root_dir=os.path.join(assets_path, "vision", "datasets", "total_segmentator"),
+            split=split,
+            split_ratios=datasets.total_segmentator.SplitRatios(0.33, 0.33, 0.33),
+            sample_every_n_slice=1,
+        )
+        ds.setup()
+        return ds
