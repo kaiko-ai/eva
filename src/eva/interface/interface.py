0f7e4bc0f244c4f3e2359fa68a92859302f3d20c
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-02-06 14:45:28 +0100

    interface updated

--- src/eva/interface/interface.py
+++ src/eva/interface/interface.py
@@ -12,26 +12,19 @@ class Interface:
     for training and validating the model using the specified data and trainer.
     """
 
-    def __init__(
+    def fit(
         self,
         model: modules.ModelModule,
         data: datamodules.DataModule,
         trainer: trainers.Trainer,
     ) -> None:
-        """Initialize the Interface class.
+        """Perform model training in place.
+
+        This method uses the specified trainer to fit the model using the provided data.
 
         Args:
             model: The model module.
             data: The data module.
             trainer: The trainer which processes the model and data.
         """
-        self._model = model
-        self._data = data
-        self._trainer = trainer
-
-    def fit(self) -> None:
-        """Perform model training in place.
-
-        This method uses the specified trainer to fit the model using the provided data.
-        """
-        self._trainer.fit(self._model, datamodule=self._data)
+        trainer.fit(model=model, datamodule=data)
