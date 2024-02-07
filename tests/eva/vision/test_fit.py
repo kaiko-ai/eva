163fbd9b503d2add1c3471f112b8bb6e66313698
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-02-07 12:08:01 +0100

    rename

--- tests/eva/vision/test_fit.py
+++ tests/eva/vision/test_fit.py
@@ -13,7 +13,7 @@ from tests.eva import _cli
         "configs/vision/tests/patch_camelyon.yaml",
     ],
 )
-def test_fit_online_workflow(configuration_file: str, lib_path: str):
+def test_fit_online(configuration_file: str, lib_path: str):
     """Tests CLI `fit` command with a given configuration."""
     _cli.run_cli_from_main(cli_args=["fit", "--config", os.path.join(lib_path, configuration_file)])
 
