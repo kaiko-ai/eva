26aa8bde73e291bcce7e35009e33849bb8c8e681
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-02-07 16:27:35 +0100

    reverted changes to test_fit

--- tests/eva/vision/test_fit.py
+++ tests/eva/vision/test_fit.py
@@ -11,20 +11,16 @@ from tests.eva import _cli
     "configuration_file",
     [
         "configs/vision/tests/patch_camelyon.yaml",
-    ],
-)
-def test_fit_online(configuration_file: str, lib_path: str):
-    """Tests CLI `fit` command with a given configuration."""
-    _cli.run_cli_from_main(cli_args=["fit", "--config", os.path.join(lib_path, configuration_file)])
-
-
-@pytest.mark.parametrize(
-    "configuration_file",
-    [
         "configs/vision/tests/precalculated_embeddings/patches.yaml",
         "configs/vision/tests/precalculated_embeddings/slides.yaml",
     ],
 )
-def test_fit_from_precalculated_embeddings(configuration_file: str, lib_path: str):
-    """Tests CLI `fit` command with a given configuration using precalculated embeddings."""
-    _cli.run_cli_from_main(cli_args=["fit", "--config", os.path.join(lib_path, configuration_file)])
+def test_fit_from_configuration(configuration_file: str, lib_path: str) -> None:
+    """Tests CLI `fit` command with a given configuration relative path."""
+    _cli.run_cli_from_main(
+        cli_args=[
+            "fit",
+            "--config",
+            os.path.join(lib_path, configuration_file),
+        ]
+    )
