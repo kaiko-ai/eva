eea1f654024fe8baeef7d95a48f431c071a9a38c
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-02-13 09:50:50 +0100

    add filename function to VisionDataset

--- src/eva/vision/data/datasets/vision.py
+++ src/eva/vision/data/datasets/vision.py
@@ -56,3 +56,17 @@ class VisionDataset(Dataset, abc.ABC, Generic[DataSample]):
     def __len__(self) -> int:
         """Returns the total length of the data."""
         raise NotImplementedError
+
+    @abc.abstractmethod
+    def filename(self, index: int) -> str:
+        """Returns the filename of the `index`'th data sample.
+
+        Note that this is the relative file path to the root.
+
+        Args:
+            index: The index of the data-sample to select.
+
+        Returns:
+            The filename of the `index`'th data sample.
+        """
+        raise NotImplementedError
