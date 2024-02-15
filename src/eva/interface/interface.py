0e0023fe3efbc3ad2771385a0728f5577fcc7a0f
roman807 <roman.moser@gmx.net>
2024-02-15 16:03:55 +0100

    added recording of results

--- src/eva/interface/interface.py
+++ src/eva/interface/interface.py
@@ -1,8 +1,13 @@
 """Main interface class."""
 
+import os
+from datetime import datetime
+
+
 from eva import trainers
 from eva.data import datamodules
 from eva.models import modules
+from eva.utils.recorder import get_run_id, record_results
 
 
 class Interface:
@@ -27,5 +32,13 @@ class Interface:
             data: The data module.
             trainer: The trainer which processes the model and data.
         """
+        start_time = datetime.now()
+        run_id = get_run_id(start_time)
         trainer.fit(model=model, datamodule=data)
-        trainer.test(datamodule=data)
+        evaluation_results = {
+            "val": trainer.validate(datamodule=data)[0],
+            "test": trainer.test(datamodule=data)[0],
+        }
+        end_time = datetime.now()
+        results_dir = os.path.join(trainer.default_root_dir, run_id)
+        record_results(evaluation_results, results_dir, start_time, end_time)
