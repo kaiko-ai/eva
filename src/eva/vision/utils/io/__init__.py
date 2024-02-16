9f9e3cdc1c1760ab9297348899c5e63ac8abb898
roman807 <roman.moser@gmx.net>
2024-02-16 13:27:49 +0100

    updated total_segmentator dataset

--- src/eva/vision/utils/io/__init__.py
+++ src/eva/vision/utils/io/__init__.py
@@ -1,6 +1,13 @@
 """Vision I/O utilities."""
 
 from eva.vision.utils.io.image import read_image
-from eva.vision.utils.io.nifti import read_nifti
+from eva.vision.utils.io.nifti import (
+    read_nifti_slice_from_image,
+    read_nifti_slice_from_image_with_unknown_dimension,
+)
 
-__all__ = ["read_image", "read_nifti"]
+__all__ = [
+    "read_image",
+    "read_nifti_slice_from_image",
+    "read_nifti_slice_from_image_with_unknown_dimension",
+]
