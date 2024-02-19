b580286c2dec6b51a5593f6ac843e89fc7f37460
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-02-19 10:22:24 +0100

    refactored predict_and_fit method

--- src/eva/interface/interface.py
+++ src/eva/interface/interface.py
@@ -4,6 +4,7 @@ from eva import trainers
 from eva.data import datamodules
 from eva.data.datamodules import schemas
 from eva.models import modules
+from eva.vision.data import datasets
 
 
 class Interface:
@@ -35,8 +36,16 @@ class Interface:
             data: The data module.
             trainer: The trainer which processes the model and data.
         """
+        if isinstance(data.datasets.train, datasets.PatchEmbeddingDataset) and isinstance(
+            model, modules.HeadModule
+        ):
+            model.backbone = None  # disable backbone when using pre-computed embeddings
+
         trainer.fit(model=model, datamodule=data)
-        trainer.test(datamodule=data)
+
+        trainer.validate(datamodule=data)
+        if data.datasets.test is not None:
+            trainer.test(datamodule=data)
 
     def predict(
         self,
@@ -78,9 +87,4 @@ class Interface:
             trainer: The trainer which processes the model and data.
         """
         self.predict(model=model, data=data, trainer=trainer)
-
-        if isinstance(model, modules.HeadModule):
-            model.backbone = None
-
-        trainer.fit(model=model, datamodule=data)
-        trainer.test(datamodule=data)
+        self.fit(model=model, data=data, trainer=trainer)
