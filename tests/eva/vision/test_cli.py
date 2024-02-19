3373b4a9a1f3b40e93b1749906a12cf0523ba552
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-02-19 10:53:37 +0100

    added pcam offline config & unit test for predict_and_fit

---
+++ tests/eva/vision/test_cli.py
@@ -0,0 +1,47 @@
+"""Tests regarding the EVA `fit` CLI command on vision datasets."""
+
+import os
+import tempfile
+from unittest.mock import patch
+
+import pytest
+
+from tests.eva import _cli
+
+
+@pytest.mark.parametrize(
+    "configuration_file",
+    [
+        "configs/vision/tests/patch_camelyon.yaml",
+        "configs/vision/tests/offline/patches.yaml",
+        "configs/vision/tests/offline/slides.yaml",
+    ],
+)
+def test_fit_from_configuration(configuration_file: str, lib_path: str) -> None:
+    """Tests CLI `fit` command with a given configuration relative path."""
+    _cli.run_cli_from_main(
+        cli_args=[
+            "fit",
+            "--config",
+            os.path.join(lib_path, configuration_file),
+        ]
+    )
+
+
+@pytest.mark.parametrize(
+    "configuration_file",
+    [
+        "configs/vision/tests/offline/patch_camelyon.yaml",
+    ],
+)
+def test_fit_and_predict_from_configuration(configuration_file: str, lib_path: str) -> None:
+    """Tests CLI `fit` command with a given configuration relative path."""
+    with tempfile.TemporaryDirectory() as output_dir:
+        with patch.dict(os.environ, {"EMBEDDINGS_ROOT": output_dir}):
+            _cli.run_cli_from_main(
+                cli_args=[
+                    "predict_and_fit",
+                    "--config",
+                    os.path.join(lib_path, configuration_file),
+                ]
+            )
