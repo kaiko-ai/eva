3680144d072009868fb20aa9888c9e71beb43e1c
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-02-19 11:15:20 +0100

    updated unit test name

--- tests/eva/vision/test_cli.py
+++ tests/eva/vision/test_cli.py
@@ -18,7 +18,7 @@ from tests.eva import _cli
     ],
 )
 def test_fit_from_configuration(configuration_file: str, lib_path: str) -> None:
-    """Tests CLI `fit` command with a given configuration relative path."""
+    """Tests CLI `fit` command with a given configuration file."""
     _cli.run_cli_from_main(
         cli_args=[
             "fit",
@@ -34,8 +34,8 @@ def test_fit_from_configuration(configuration_file: str, lib_path: str) -> None:
         "configs/vision/tests/offline/patch_camelyon.yaml",
     ],
 )
-def test_fit_and_predict_from_configuration(configuration_file: str, lib_path: str) -> None:
-    """Tests CLI `fit` command with a given configuration relative path."""
+def test_predict_fit_from_configuration(configuration_file: str, lib_path: str) -> None:
+    """Tests CLI `predict_fit` command with a given configuration file."""
     with tempfile.TemporaryDirectory() as output_dir:
         with patch.dict(os.environ, {"EMBEDDINGS_ROOT": output_dir}):
             _cli.run_cli_from_main(
