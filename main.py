bcf714b8903bdedfecd0930a07bffdacac51586d
ioangatop <johngatop@gmail.com>
2024-02-28 14:22:00 +0100

    remove dev files

--- main.py
+++
@@ -1,7 +0,0 @@
-from eva.vision.data.datasets import CRC_HE, CRC_HE_NONORM
-
-dataset = CRC_HE("data/crc_he", split="train", download=True)
-dataset.prepare_data()
-
-dataset = CRC_HE_NONORM("data/crc_he", split="train", download=True)
-dataset.prepare_data()
