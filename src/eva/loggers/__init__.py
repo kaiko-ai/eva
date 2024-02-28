5dcf9636e81afb5e65f5dbf965f2b758e2e57c62
roman807 <roman.moser@gmx.net>
2024-02-28 15:07:02 +0100

    refactored loggers

--- src/eva/loggers/__init__.py
+++ src/eva/loggers/__init__.py
@@ -1,6 +1,6 @@
 """Loggers API."""
 
-from eva.loggers.csv import CSVLogger
-from eva.loggers.tensorboard import TensorBoardLogger
+from eva.loggers.lightning_loggers import CSVLogger
+from eva.loggers.lightning_loggers import TensorBoardLogger
 
 __all__ = ["CSVLogger", "TensorBoardLogger"]
---
+++ src/eva/loggers/lightning_loggers.py
@@ -0,0 +1,39 @@
+"""Custom logger classes for PyTorch Lightning."""
+
+from pytorch_lightning import loggers
+from typing_extensions import override
+
+
+class BaseLogger(loggers.Logger):
+    """Base logger class."""
+    def __init__(self, *args, **kwargs):
+        """Initializes the BaseLogger instance.
+        
+        Overwrites the parent class to allow for custom log_dir setting.
+        """
+        super().__init__(*args, **kwargs)
+        self._log_dir = None
+
+    @property
+    @override
+    def log_dir(self) -> str:
+        if self._log_dir is not None:
+            return self._log_dir
+        else:
+            return super().log_dir
+
+    @log_dir.setter
+    def log_dir(self, value):
+        self._log_dir = value
+
+
+class TensorBoardLogger(BaseLogger, loggers.TensorBoardLogger):
+    """TensorBoard logger class."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class CSVLogger(BaseLogger, loggers.CSVLogger):
+    """CSV logger class."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
