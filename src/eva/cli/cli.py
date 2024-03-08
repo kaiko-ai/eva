a0d291f56d9526597ed69b46c7985668b5737fc0
ioangatop <johngatop@gmail.com>
2024-03-08 15:05:09 +0100

    add set random seed functionality

--- src/eva/cli/cli.py
+++ src/eva/cli/cli.py
@@ -3,12 +3,10 @@
 import jsonargparse
 
 from eva import interface
-from eva.cli import _logo
 
 
 def cli() -> object:
     """Main CLI factory."""
-    _logo.print_cli_logo()
     return jsonargparse.CLI(
         interface.Interface,
         parser_mode="omegaconf",
--- src/eva/setup.py
+++ src/eva/setup.py
@@ -5,7 +5,22 @@ import sys
 import warnings
 
 import jsonargparse
+from lightning_fabric.utilities import seed as pl_seed
 from loguru import logger
+
+from eva.utils import logo, workers
+
+
+def _configure_random_seed(seed: int | None = None) -> None:
+    """Sets the global random seed.
+
+    Args:
+        seed: The seed number to use. If `None`, it will read the seed from
+            `EVA_GLOBAL_SEED` env variable. If `None` and the `EVA_GLOBAL_SEED`
+            env variable is not set, then the seed defaults to `42`.
+    """
+    effective_seed = seed or os.environ.get("EVA_GLOBAL_SEED", default=42)
+    pl_seed.seed_everything(seed=int(effective_seed), workers=True)
 
 
 def _configure_jsonargparse() -> None:
@@ -51,8 +66,11 @@ def _enable_mps_fallback() -> None:
         os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 
+@workers.main_worker_only
 def setup() -> None:
     """Sets up the environment before the module is imported."""
+    logo.print_cli_logo()
+    _configure_random_seed()
     _configure_jsonargparse()
     _initialize_logger()
     _suppress_warnings()
