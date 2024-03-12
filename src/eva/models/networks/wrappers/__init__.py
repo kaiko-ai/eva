ad56f699555159898cefa1950f235199c0ea0d8f
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-03-12 15:08:47 +0100

    added missing entry to init

--- src/eva/models/networks/wrappers/__init__.py
+++ src/eva/models/networks/wrappers/__init__.py
@@ -1,7 +1,8 @@
 """Model Wrappers API."""
 
+from eva.models.networks.wrappers.base import BaseModel
 from eva.models.networks.wrappers.from_function import ModelFromFunction
 from eva.models.networks.wrappers.huggingface import HuggingFaceModel
 from eva.models.networks.wrappers.onnx import ONNXModel
 
-__all__ = ["ModelFromFunction", "HuggingFaceModel", "ONNXModel"]
+__all__ = ["BaseModel", "ModelFromFunction", "HuggingFaceModel", "ONNXModel"]
