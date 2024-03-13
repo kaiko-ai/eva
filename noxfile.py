c1e157fa4a9286367ce1232bdff27b185461a27f
ioangatop <johngatop@gmail.com>
2024-03-13 13:56:38 +0100

    Add support for version bum

--- noxfile.py
+++ noxfile.py
@@ -93,3 +93,24 @@ def ci(session: nox.Session) -> None:
     session.notify("lint")
     session.notify("check")
     session.notify("test")
+
+
+@nox.session
+def bump(session: nox.Session) -> None:
+    """Bumps the version of the library.
+
+    Usage:
+      Update patch (0.0.1 -> 0.0.2)
+      >>> nox -s bump -- micro
+
+      Update minor (0.0.1 -> 0.1.0)
+      >>> nox -s bump -- minor
+
+      Update major (0.0.1 -> 1.0.0)
+      >>> nox -s bump -- minor
+
+      Update dev (0.0.1 -> 0.0.1.dev1)
+      >>> nox -s bump -- dev
+    """
+    session.run_always("pdm", "self", "add", "pdm-bump", external=True)
+    session.run("pdm", "bump", *session.posargs, external=True)
