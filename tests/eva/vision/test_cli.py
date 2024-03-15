b71fcf4229a6ab88cbf4f88091bf6bae04c2bf78
ioangatop <johngatop@gmail.com>
2024-03-15 22:20:59 +0100

    updates

--- tests/eva/vision/test_cli.py
+++ tests/eva/vision/test_cli.py
@@ -9,25 +9,6 @@ import pytest
 from eva.vision.data import datasets
 from tests.eva import _cli
 
-<<<<<<< HEAD
-# @pytest.mark.parametrize(
-#     "configuration_file",
-#     [
-#         "configs/vision/tests/online/patch_camelyon.yaml",
-#         "configs/vision/tests/offline/patches.yaml",
-#         "configs/vision/tests/offline/slides.yaml",
-#     ],
-# )
-# def test_fit_from_configuration(configuration_file: str, lib_path: str) -> None:
-#     """Tests CLI `fit` command with a given configuration file."""
-#     _cli.run_cli_from_main(
-#         cli_args=[
-#             "fit",
-#             "--config",
-#             os.path.join(lib_path, configuration_file),
-#         ]
-#     )
-=======
 
 @pytest.mark.parametrize(
     "configuration_file",
@@ -47,7 +28,6 @@ def test_fit_from_configuration(configuration_file: str, lib_path: str) -> None:
             os.path.join(lib_path, configuration_file),
         ]
     )
->>>>>>> main
 
 
 @pytest.mark.parametrize(
