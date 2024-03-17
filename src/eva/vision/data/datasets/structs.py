c262fa3098d9329993954aecd241073564dfc704
ioangatop <johngatop@gmail.com>
2024-03-17 15:56:28 +0100

    Remove `SplitRatios` dataclass

--- src/eva/vision/data/datasets/structs.py
+++ src/eva/vision/data/datasets/structs.py
@@ -15,17 +15,3 @@ class DownloadResource:
 
     md5: str | None = None
     """The MD5 hash of the resource."""
-
-
-@dataclasses.dataclass(frozen=True)
-class SplitRatios:
-    """Contains split ratios for train, val and test."""
-
-    train: float
-    """Train split ratio."""
-
-    val: float
-    """Validation split ratio."""
-
-    test: float
-    """Test split ratio."""
