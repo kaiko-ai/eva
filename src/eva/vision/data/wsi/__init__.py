278b7581de5ba4e68a5917b9ae65f2d1a621562a
Nicolas Känzig <36882833+nkaenzig@users.noreply.github.com>
2024-04-25 14:36:02 +0200

    Replaced cached_property in WsiDataset by LRU cache (#388)

--- src/eva/vision/data/wsi/__init__.py
+++ src/eva/vision/data/wsi/__init__.py
@@ -1,6 +1,6 @@
 """WSI API."""
 
-from eva.vision.data.wsi.backends import Wsi, wsi_backend
-from eva.vision.data.wsi.patching.coordinates import PatchCoordinates
+from eva.vision.data.wsi.backends import Wsi, get_cached_wsi, wsi_backend
+from eva.vision.data.wsi.patching.coordinates import PatchCoordinates, get_cached_coords
 
-__all__ = ["Wsi", "PatchCoordinates", "wsi_backend"]
+__all__ = ["Wsi", "PatchCoordinates", "get_cached_coords", "wsi_backend", "get_cached_wsi"]
