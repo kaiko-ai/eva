65dd8af02b9b2d84c78387c4b357763d53fdae11
ioangatop <johngatop@gmail.com>
2024-05-16 14:38:20 +0200

    Remove total segmentator classification dataset (#450)

--- src/eva/vision/data/datasets/__init__.py
+++ src/eva/vision/data/datasets/__init__.py
@@ -1,12 +1,6 @@
 """Vision Datasets API."""
 
-from eva.vision.data.datasets.classification import (
-    BACH,
-    CRC,
-    MHIST,
-    PatchCamelyon,
-    TotalSegmentatorClassification,
-)
+from eva.vision.data.datasets.classification import BACH, CRC, MHIST, PatchCamelyon
 from eva.vision.data.datasets.segmentation import ImageSegmentation, TotalSegmentator2D
 from eva.vision.data.datasets.vision import VisionDataset
 
@@ -16,7 +10,6 @@ __all__ = [
     "MHIST",
     "ImageSegmentation",
     "PatchCamelyon",
-    "TotalSegmentatorClassification",
     "TotalSegmentator2D",
     "VisionDataset",
 ]
