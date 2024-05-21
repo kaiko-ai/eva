e3e5671eb24a2d134d9efdeb5e01891a921547d6
Nicolas Känzig <36882833+nkaenzig@users.noreply.github.com>
2024-05-21 08:48:24 +0200

    update WSI foreground segmentation algorithm (#456)

--- src/eva/vision/data/wsi/__init__.py
+++ src/eva/vision/data/wsi/__init__.py
@@ -2,5 +2,15 @@
 
 from eva.vision.data.wsi.backends import Wsi, get_cached_wsi, wsi_backend
 from eva.vision.data.wsi.patching.coordinates import PatchCoordinates, get_cached_coords
+from eva.vision.data.wsi.patching.mask import Mask, get_mask, get_mask_level
 
-__all__ = ["Wsi", "PatchCoordinates", "get_cached_coords", "wsi_backend", "get_cached_wsi"]
+__all__ = [
+    "Wsi",
+    "PatchCoordinates",
+    "Mask",
+    "get_cached_coords",
+    "wsi_backend",
+    "get_cached_wsi",
+    "get_mask",
+    "get_mask_level",
+]
