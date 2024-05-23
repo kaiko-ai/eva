22a2a9288c55adbf68e0da1e9e588173b388d0a1
ioangatop <johngatop@gmail.com>
2024-05-23 15:35:57 +0200

    update parsing method

--- src/eva/core/callbacks/config.py
+++ src/eva/core/callbacks/config.py
@@ -1,6 +1,5 @@
 """Configuration logger callback."""
 
-import ast
 import os
 import sys
 from types import BuiltinFunctionType
@@ -141,7 +140,7 @@ def _type_resolver(mapping: Dict[str, Any]) -> Dict[str, Any]:
             formatted_value = [_type_resolver(subvalue) for subvalue in value]
         else:
             try:
-                parsed_value = ast.literal_eval(value)  # type: ignore
+                parsed_value = eval(value)  # type: ignore # nosec
                 formatted_value = (
                     value if isinstance(parsed_value, BuiltinFunctionType) else parsed_value
                 )
