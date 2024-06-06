218dba364a701376370f7f0be9995b7f9d71a2aa
ioangatop <johngatop@gmail.com>
2024-06-06 14:16:32 +0200

    updates

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from eva.vision.data.datasets.segmentation.total_segmentator import TotalSegmentator2D
+from eva.vision.data.datasets import segmentation
 
-dataset = TotalSegmentator2D(
-    root="data/total_segmentator",
+# dataset = segmentation.TotalSegmentator2D(
+#     root="data/total_segmentator",
+#     split="train",
+#     download=False,
+# )
+# dataset.setup()
+
+dataset = segmentation.EmbeddingsSegmentationDataset(
+    root="./data/embeddings/dino_vits16/total_segmentator_2d",
     split="train",
-    download=False,
+    manifest_file="manifest.csv",
 )
 dataset.setup()
 
-
-for i in range(1000):
-    filename = dataset.filename(i)
-    print(filename)
-
+for i in range(100):
+    data, targets = dataset[i]
+    print(data)
+    print(targets)
+    quit()
