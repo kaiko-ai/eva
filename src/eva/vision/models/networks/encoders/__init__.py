914ecdf8ef9519a83ffbfc6290dc1db52314b5bd
roman807 <roman.moser@gmx.net>
2024-06-28 17:06:21 +0200

    `EncoderWrapper` model and fixes for segmentation tasks (#551)
    * wrapper for visiontransformer
    
    * update feature extractor
    
    * minor updates
    
    ---------
    
    Co-authored-by: ioangatop <johngatop@gmail.com>

--- src/eva/vision/models/networks/encoders/__init__.py
+++ src/eva/vision/models/networks/encoders/__init__.py
@@ -2,5 +2,6 @@
 
 from eva.vision.models.networks.encoders.encoder import Encoder
 from eva.vision.models.networks.encoders.from_timm import TimmEncoder
+from eva.vision.models.networks.encoders.wrapper import EncoderWrapper
 
-__all__ = ["Encoder", "TimmEncoder"]
+__all__ = ["Encoder", "TimmEncoder", "EncoderWrapper"]
