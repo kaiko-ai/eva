b581bcfa015c4b4f70766096f2f729a99706118d
ioangatop <johngatop@gmail.com>
2024-07-03 16:46:25 +0200

    fixes

--- src/eva/vision/data/datasets/__init__.py
+++ src/eva/vision/data/datasets/__init__.py
@@ -11,6 +11,7 @@ from eva.vision.data.datasets.classification import (
 )
 from eva.vision.data.datasets.segmentation import (
     CoNSeP,
+    EmbeddingsSegmentationDataset,
     ImageSegmentation,
     MoNuSAC,
     TotalSegmentator2D,
@@ -27,6 +28,7 @@ __all__ = [
     "PatchCamelyon",
     "WsiClassificationDataset",
     "CoNSeP",
+    "EmbeddingsSegmentationDataset",
     "ImageSegmentation",
     "MoNuSAC",
     "TotalSegmentator2D",
