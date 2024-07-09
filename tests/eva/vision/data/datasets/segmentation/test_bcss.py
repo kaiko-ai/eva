2d510141159cd58a52f654ce099f550a9c3d3175
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-07-09 11:45:35 +0200

    added unit tests

---
+++ tests/eva/vision/data/datasets/segmentation/test_bcss.py
@@ -0,0 +1,69 @@
+"""PANDA dataset tests."""
+
+import os
+from typing import Literal
+from unittest.mock import PropertyMock, patch
+
+import pytest
+from torchvision import tv_tensors
+
+from eva.vision.data import datasets
+from eva.vision.data.wsi.patching import samplers
+
+
+@pytest.mark.parametrize(
+    "split, expected_length",
+    [("train", 80), ("val", 28), ("test", 16), (None, 124)],
+)
+def test_length(dataset: datasets.BCSS, expected_length: int) -> None:
+    """Tests the length of the dataset."""
+    assert len(dataset) == expected_length
+
+
+@pytest.mark.parametrize(
+    "split, index",
+    [
+        (None, 0),
+        ("train", 0),
+        ("val", 0),
+    ],
+)
+def test_sample(dataset: datasets.BCSS, index: int) -> None:
+    """Tests the format of a dataset sample."""
+    # assert data sample is a tuple
+    sample = dataset[index]
+    assert isinstance(sample, tuple)
+    assert len(sample) == 2
+    # assert the format of the `image` and `mask`
+    image, mask = sample
+    assert isinstance(image, tv_tensors.Image)
+    assert image.shape == (3, 10, 10)
+    assert isinstance(mask, tv_tensors.Mask)
+    assert mask.shape == (10, 10)
+
+
+@pytest.fixture
+def root(assets_path: str) -> str:
+    """Fixture returning the root directory of the dataset."""
+    return os.path.join(assets_path, "vision/datasets/bcss")
+
+
+@pytest.fixture(scope="function")
+def dataset(split: Literal["train", "val"] | None, root: str) -> datasets.BCSS:
+    """BCSS dataset fixture."""
+    with patch.object(
+        datasets.BCSS,
+        "_expected_length",
+        new_callable=PropertyMock(return_value=10),
+    ):
+        dataset = datasets.BCSS(
+            root=root,
+            split=split,
+            width=10,
+            height=10,
+            target_mpp=0.25,
+            sampler=samplers.GridSampler(),
+        )
+        dataset.prepare_data()
+        dataset.setup()
+        return dataset
