bb30ae87c4fdc838ea41e56dce19440b92a546bc
ioangatop <johngatop@gmail.com>
2024-07-09 14:06:38 +0200

    rm unused functions

--- src/eva/core/callbacks/writers/embeddings/segmentation.py
+++ src/eva/core/callbacks/writers/embeddings/segmentation.py
@@ -74,32 +74,8 @@ class SegmentationEmbeddingsWriter(base.EmbeddingsWriter):
         return embeddings
 
 
-def _init_manifest(output_dir: str, overwrite: bool = False) -> tuple[io.TextIOWrapper, Any]:
-    manifest_path = os.path.join(output_dir, "manifest.csv")
-    if os.path.exists(manifest_path) and not overwrite:
-        raise FileExistsError(
-            f"Manifest file already exists at {manifest_path}. This likely means that the "
-            "embeddings have been computed before. Consider using `eva fit` instead "
-            "of `eva predict_fit` or `eva predict`."
-        )
-    manifest_file = open(manifest_path, "w", newline="")
-    manifest_writer = csv.writer(manifest_file)
-    manifest_writer.writerow(["origin", "embeddings", "target", "split"])
-    return manifest_file, manifest_writer
-
-
 def _save_embedding(embeddings_buffer: io.BytesIO, save_name: str, output_dir: str) -> None:
     save_path = os.path.join(output_dir, save_name)
     prediction = torch.load(io.BytesIO(embeddings_buffer.getbuffer()), map_location="cpu")
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     torch.save(prediction, save_path)
-
-
-def _update_manifest(
-    target_filename: str,
-    input_name: str,
-    save_name: str,
-    split: str | None,
-    manifest_writer,
-) -> None:
-    manifest_writer.writerow([input_name, save_name, target_filename, split])
