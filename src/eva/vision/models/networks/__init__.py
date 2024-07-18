6c8bf651ab4e12b69cf00c983e8d670ae18d685e
ioangatop <johngatop@gmail.com>
2024-07-18 14:30:40 +0200

    fix encoder

--- src/eva/vision/models/networks/__init__.py
+++ src/eva/vision/models/networks/__init__.py
@@ -3,6 +3,5 @@
 from eva.vision.models.networks import postprocesses
 from eva.vision.models.networks.abmil import ABMIL
 from eva.vision.models.networks.from_timm import TimmModel
-from eva.vision.models.networks.phikon import Phikon
 
-__all__ = ["postprocesses", "ABMIL", "TimmModel", "Phikon"]
+__all__ = ["postprocesses", "ABMIL", "TimmModel"]
--- src/eva/vision/models/networks/encoders/__init__.py
+++ src/eva/vision/models/networks/encoders/__init__.py
@@ -2,7 +2,6 @@
 
 from eva.vision.models.networks.encoders.encoder import Encoder
 from eva.vision.models.networks.encoders.from_timm import TimmEncoder
-from eva.vision.models.networks.encoders.phikon import PhikonEncoder
 from eva.vision.models.networks.encoders.wrapper import EncoderWrapper
 
-__all__ = ["Encoder", "TimmEncoder", "PhikonEncoder", "EncoderWrapper"]
+__all__ = ["Encoder", "TimmEncoder", "EncoderWrapper"]
