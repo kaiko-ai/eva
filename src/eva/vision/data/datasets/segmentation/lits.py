ec508f3445b8765816203c882e12828e8fd289fe
ioangatop <johngatop@gmail.com>
2024-07-22 16:20:30 +0200

    add background as class

--- src/eva/vision/data/datasets/segmentation/lits.py
+++ src/eva/vision/data/datasets/segmentation/lits.py
@@ -71,7 +71,7 @@ class LiTS(base.ImageSegmentation):
     @property
     @override
     def classes(self) -> List[str]:
-        return ["liver", "tumor"]
+        return ["background", "liver", "tumor"]
 
     @functools.cached_property
     @override
