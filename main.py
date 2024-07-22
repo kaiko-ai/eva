a26784189310af67f05add09ee62d15b032fdff6
ioangatop <johngatop@gmail.com>
2024-07-22 16:15:31 +0200

    udpates

---
+++ main.py
@@ -0,0 +1,45 @@
+"""Helper wrapper class for Pathology FMs."""
+
+from typing import Any
+
+import torch
+from torch import nn
+
+
+def load_pathology_fm(model_name: str, **kwargs: Any) -> nn.Module:
+    """."""
+    pathology_fm_lib = {
+        "kaiko_vits16": torch.hub.load(
+            repo_or_dir="kaiko-ai/towards_large_pathology_fms",
+            model="vits16",
+            trust_repo=True,
+            **kwargs,
+        ),
+        "kaiko_vits8": torch.hub.load(
+            repo_or_dir="kaiko-ai/towards_large_pathology_fms",
+            model="vits8",
+            trust_repo=True,
+            **kwargs,
+        ),
+        "kaiko_vitb16": torch.hub.load(
+            repo_or_dir="kaiko-ai/towards_large_pathology_fms",
+            model="vitb16",
+            trust_repo=True,
+            **kwargs,
+        ),
+        "kaiko_vitb8": torch.hub.load(
+            repo_or_dir="kaiko-ai/towards_large_pathology_fms",
+            model="vitb8",
+            trust_repo=True,
+            **kwargs,
+        ),
+    }
+    model = pathology_fm_lib.get(model_name)
+    if model is None:
+        raise ValueError("")
+
+    return model
+
+
+model = load_pathology_fm(model_name="kaiko_vits16")
+print(model)
