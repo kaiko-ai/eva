b8f0784b162c1af785c74763d00876153cdc7ef2
ioangatop <johngatop@gmail.com>
2024-07-29 14:54:18 +0200

    Fix `ignore_index` operation

--- src/eva/vision/losses/dice.py
+++ src/eva/vision/losses/dice.py
@@ -32,8 +32,7 @@ class DiceLoss(losses.DiceLoss):  # type: ignore
         if self.ignore_index is not None:
             mask = targets != self.ignore_index
             targets = targets * mask
-            inputs_mask = (mask.unsqueeze(1) if mask.ndim == 3 else mask)
-            inputs = inputs * inputs_mask.expand(inputs.size())
+            inputs = torch.mul(inputs, mask.unsqueeze(1) if mask.ndim == 3 else mask)
 
         if targets.ndim == 3:
             targets = one_hot(targets[:, None, ...], num_classes=inputs.shape[1])
