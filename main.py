0bc3aea0852b09d32852b7ee3f00d44fa3f8c67d
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-08-07 08:47:06 +0200

    remove main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-"""Helper wrapper class for Pathology FMs."""
-
-from eva.vision.models import backbones
-
-# model = backbones.BackboneModelRegistry.load_model("pathology/kaiko_vits16")
-model = backbones.BackboneModelRegistry.load_model("pathology/owkin_phikon")
-print(type(model))
