6fbd0187a55f674c49c7fbbee5d053db92f4eac6
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-09-11 10:31:00 +0200

    cleanup

--- src/eva/vision/models/networks/adapters/__init__.py
+++
@@ -1,3 +0,0 @@
-from eva.vision.models.networks.adapters.vit_adapter import ViTAdapter
-
-__all__ = ["ViTAdapter"]
--- src/eva/vision/models/networks/adapters/vit_adapter/__init__.py
+++
@@ -1,5 +0,0 @@
-"""ViT-Adapter modules."""
-
-from eva.vision.models.networks.adapters.vit_adapter.vit_adapter import ViTAdapter
-
-__all__ = ["ViTAdapter"]
