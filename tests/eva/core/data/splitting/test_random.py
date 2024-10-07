8edee37ecabb810f1d5569f6e34804e40a12d7e1
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-10-07 11:26:07 +0000

    updated random split logic & test

--- tests/eva/core/data/splitting/test_random.py
+++ tests/eva/core/data/splitting/test_random.py
@@ -1,6 +1,7 @@
 """Tests for the random split function."""
 
 import pytest
+from typing import List
 
 from eva.core.data import splitting
 
@@ -32,11 +33,11 @@ def test_split_ratios(n_samples: int, train_ratio: float, val_ratio: float, test
     assert len(train_indices) + len(val_indices) + len(test_indices or []) == n_samples
 
 
-@pytest.mark.parametrize("train_ratio, val_ratio, test_ratio", [(0.6, 0.3, 0.0), (0.6, 0.4, 0.3)])
+@pytest.mark.parametrize("train_ratio, val_ratio, test_ratio", [(0.6, 0.7, 0.0), (0.6, 0.4, 0.3)])
 def test_invalid_ratio_sums(train_ratio: float, val_ratio: float, test_ratio: float):
     """Tests if the function raises an error when the ratios do not sum to 1."""
     samples = list(range(100))
-    expected_error = "The sum of the ratios must be equal to 1."
+    expected_error = "The sum of the ratios must be lower or equal to 1"
     with pytest.raises(ValueError, match=expected_error):
         splitting.random_split(samples, train_ratio, val_ratio, test_ratio)
 
@@ -53,8 +54,20 @@ def test_different_seeds_produce_different_outputs(seed1, seed2):
     assert test1 != test2, "Different seeds should produce different test indices"
 
 
-@pytest.mark.parametrize("seed", [42, 123, 999])
-def test_same_seed_produces_same_outputs(seed):
+@pytest.mark.parametrize(
+    "seed, train_expected_indices, val_expected_indices, test_expected_indices",
+    [
+        (42, [59, 21, 56, 18], [69, 15, 48, 55], [49, 6, 90, 11]),
+        (123, [21, 71, 92, 23], [89, 14, 64, 4], [45, 75, 62, 6]),
+        (999, [47, 42, 57, 50], [41, 3, 81, 61], [45, 6, 56, 67]),
+    ],
+)
+def test_same_seed_produces_same_outputs(
+    seed: int,
+    train_expected_indices: List[int],
+    val_expected_indices: List[int],
+    test_expected_indices: List[int],
+):
     """Tests if the same seed produces the same train, validation, and test indices."""
     samples = list(range(100))
     train1, val1, test1 = splitting.random_split(samples, 0.6, 0.2, 0.2, seed=seed)
@@ -63,6 +76,11 @@ def test_same_seed_produces_same_outputs(seed):
     assert train1 == train2, "Same seed should produce the same train indices"
     assert val1 == val2, "Same seed should produce the same validation indices"
     assert test1 == test2, "Same seed should produce the same test indices"
+    assert isinstance(test1, list)
+
+    assert train1[: len(train_expected_indices)] == train_expected_indices, "Unexpected indices"
+    assert val1[: len(val_expected_indices)] == val_expected_indices, "Unexpected indices"
+    assert test1[: len(test_expected_indices)] == test_expected_indices, "Unexpected indices"
 
 
 def test_no_test_set():
