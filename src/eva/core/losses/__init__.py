209b6947859ccd7f01b480acd319b3d9b1858c3c
Nicolas Känzig <36882833+nkaenzig@users.noreply.github.com>
2024-10-14 13:20:30 +0200

    Add `CrossEntropyLoss` and `DiceCELoss` wrappers with support for class weights (#686)

---
+++ src/eva/core/losses/__init__.py
@@ -0,0 +1,5 @@
+"""Loss functions API."""
+
+from eva.core.losses.cross_entropy import CrossEntropyLoss
+
+__all__ = ["CrossEntropyLoss"]
--- src/eva/vision/losses/__init__.py
+++ src/eva/vision/losses/__init__.py
@@ -1,5 +1,5 @@
 """Loss functions API."""
 
-from eva.vision.losses.dice import DiceLoss
+from eva.vision.losses.dice import DiceCELoss, DiceLoss
 
-__all__ = ["DiceLoss"]
+__all__ = ["DiceLoss", "DiceCELoss"]
