a32340fa929433a521f75da216cdd72c4a6971b8
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-11-29 15:35:15 +0100

    add torch.hub models to model registry

--- src/eva/core/models/wrappers/__init__.py
+++ src/eva/core/models/wrappers/__init__.py
@@ -2,9 +2,9 @@
 
 from eva.core.models.wrappers.base import BaseModel
 from eva.core.models.wrappers.from_function import ModelFromFunction
+from eva.core.models.wrappers.from_torchhub import TorchHubModel
 from eva.core.models.wrappers.huggingface import HuggingFaceModel
 from eva.core.models.wrappers.onnx import ONNXModel
-from eva.core.models.wrappers.from_torchhub import TorchHubModel
 
 __all__ = [
     "BaseModel",
