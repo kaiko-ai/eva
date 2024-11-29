ab69fcc69c6e3b3c4428d01eb8f0fd42adb7b8c7
Nicolas Kaenzig <nkaenzig@gmail.com>
2024-11-29 13:39:06 +0100

    updated yaml configs

--- src/eva/vision/metrics/__init__.py
+++ src/eva/vision/metrics/__init__.py
@@ -4,10 +4,12 @@ from eva.vision.metrics.defaults.segmentation import MulticlassSegmentationMetri
 from eva.vision.metrics.segmentation.dice import DiceScore
 from eva.vision.metrics.segmentation.generalized_dice import GeneralizedDiceScore
 from eva.vision.metrics.segmentation.mean_iou import MeanIoU
+from eva.vision.metrics.segmentation.monai_dice import MonaiDiceScore
 
 __all__ = [
     "DiceScore",
     "GeneralizedDiceScore",
-    "MulticlassSegmentationMetrics",
     "MeanIoU",
+    "MonaiDiceScore",
+    "MulticlassSegmentationMetrics",
 ]
