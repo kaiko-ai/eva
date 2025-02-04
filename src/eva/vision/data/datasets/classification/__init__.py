2f75163a37cc0c622fca4671ad935d76dfac2c9c
Nicolas Känzig <36882833+nkaenzig@users.noreply.github.com>
2025-02-04 10:48:24 +0100

    Add `GleasonArvaniti` dataset (#754)

--- src/eva/vision/data/datasets/classification/__init__.py
+++ src/eva/vision/data/datasets/classification/__init__.py
@@ -3,6 +3,7 @@
 from eva.vision.data.datasets.classification.bach import BACH
 from eva.vision.data.datasets.classification.camelyon16 import Camelyon16
 from eva.vision.data.datasets.classification.crc import CRC
+from eva.vision.data.datasets.classification.gleason_arvaniti import GleasonArvaniti
 from eva.vision.data.datasets.classification.mhist import MHIST
 from eva.vision.data.datasets.classification.panda import PANDA, PANDASmall
 from eva.vision.data.datasets.classification.patch_camelyon import PatchCamelyon
@@ -10,11 +11,12 @@ from eva.vision.data.datasets.classification.wsi import WsiClassificationDataset
 
 __all__ = [
     "BACH",
+    "Camelyon16",
     "CRC",
+    "GleasonArvaniti",
     "MHIST",
     "PatchCamelyon",
     "WsiClassificationDataset",
     "PANDA",
     "PANDASmall",
-    "Camelyon16",
 ]
