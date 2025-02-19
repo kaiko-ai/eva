3f1e239f8fc16275f54c40c6cec7247d339a72d1
Rita <ritakurban@Ritas-Mac.local>
2025-02-19 15:06:41 +0000

    Add litellm

--- src/eva/language/models/wrappers/__init__.py
+++ src/eva/language/models/wrappers/__init__.py
@@ -1,5 +1,6 @@
 """Language Model Wrappers API."""
 
 from eva.language.models.wrappers.huggingface import HuggingFaceTextModel
+from eva.language.models.wrappers.litellm import LiteLLMTextModel
 
-__all__ = ["HuggingFaceTextModel"]
+__all__ = ["HuggingFaceTextModel", "LiteLLMTextModel"]
