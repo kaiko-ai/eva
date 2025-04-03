e9071a716a37cc19863f683dce45b65d3cc42c89
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-04-03 09:27:15 +0200

    updated unit test

--- tests/eva/vision/models/modules/test_semantic_segmentation.py
+++ tests/eva/vision/models/modules/test_semantic_segmentation.py
@@ -1,5 +1,7 @@
 """Tests the HeadModule module."""
 
+from unittest import mock
+
 import pytest
 import torch
 from monai.inferers.inferer import SimpleInferer
@@ -33,49 +35,59 @@ def test_semantic_segmentation_module_fit(
     assert not torch.all(torch.eq(initial_decoder_weights, model.decoder._layers.weight))
 
 
-def test_semantic_segmentation_module_with_inferer(
+def test_semantic_segmentation_module_forward_with_inferer(
     model_with_inferer: modules.SemanticSegmentationModule,
     segmentation_dataset: datasets.TorchDataset,
 ) -> None:
-    """Tests the SemanticSegmentationModule with inferer for inference."""
+    """Tests if the forward pass uses the inferer when in eval mode."""
     # Get a sample from dataset
     sample_data, _ = segmentation_dataset[0]
-    sample_data = sample_data.unsqueeze(0)  # Add batch dimension
+    sample_data = sample_data.unsqueeze(0)
 
-    # Run inference with the inferer
-    model_with_inferer.eval()
-    output = model_with_inferer(sample_data)
+    # Execute the forward pass in train & eval mode
+    assert model_with_inferer.inferer is not None, "Inferer should not be None"
+    with mock.patch.object(
+        type(model_with_inferer.inferer), "__call__", wraps=model_with_inferer.inferer.__call__
+    ) as mock_inferer_call:
+        output = model_with_inferer(sample_data)
+        mock_inferer_call.assert_not_called()
 
-    # Check output shape
-    assert output.shape[0] == 1  # Batch size of 1
-    assert output.shape[1] == 4  # Number of classes
-    assert output.shape[2:] == (16, 16)  # H, W of input
+        model_with_inferer.eval()
+        output = model_with_inferer(sample_data)
+        mock_inferer_call.assert_called_once()
+
+    assert output.shape[0] == 1
+    assert output.shape[1] == 4
+    assert output.shape[2:] == (16, 16)
+
+
+model_kwargs = {
+    "decoder": segmentation.Decoder2D(
+        layers=nn.Conv2d(
+            in_channels=192,
+            out_channels=4,
+            kernel_size=(1, 1),
+        ),
+    ),
+    "criterion": nn.CrossEntropyLoss(),
+    "encoder": wrappers.TimmModel(
+        model_name="vit_tiny_patch16_224",
+        pretrained=False,
+        out_indices=1,
+        model_kwargs={
+            "dynamic_img_size": True,
+        },
+    ),
+    "metrics": metrics.MetricsSchema(
+        common=metrics.AverageLoss(),
+    ),
+}
 
 
 @pytest.fixture(scope="function")
-def model(n_classes: int = 4) -> modules.SemanticSegmentationModule:
+def model() -> modules.SemanticSegmentationModule:
     """Returns a SemanticSegmentationModule model fixture."""
-    return modules.SemanticSegmentationModule(
-        decoder=segmentation.Decoder2D(
-            layers=nn.Conv2d(
-                in_channels=192,
-                out_channels=n_classes,
-                kernel_size=(1, 1),
-            ),
-        ),
-        criterion=nn.CrossEntropyLoss(),
-        encoder=wrappers.TimmModel(
-            model_name="vit_tiny_patch16_224",
-            pretrained=False,
-            out_indices=1,
-            model_kwargs={
-                "dynamic_img_size": True,
-            },
-        ),
-        metrics=metrics.MetricsSchema(
-            common=metrics.AverageLoss(),
-        ),
-    )
+    return modules.SemanticSegmentationModule(**model_kwargs)
 
 
 @pytest.fixture(scope="function")
@@ -83,5 +95,4 @@ def model_with_inferer(
     model: modules.SemanticSegmentationModule,
 ) -> modules.SemanticSegmentationModule:
     """Returns a SemanticSegmentationModule model fixture with an inferer."""
-    model.inferer = SimpleInferer()
-    return model
+    return modules.SemanticSegmentationModule(**model_kwargs, inferer=SimpleInferer())
