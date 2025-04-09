89737c16def5f124254c451a597809fbbdeebaa5
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-04-09 16:53:12 +0200

    add swin unetr decoder

--- src/eva/vision/models/networks/decoders/segmentation/semantic/__init__.py
+++ src/eva/vision/models/networks/decoders/segmentation/semantic/__init__.py
@@ -5,8 +5,15 @@ from eva.vision.models.networks.decoders.segmentation.semantic.common import (
     ConvDecoderMS,
     SingleLinearDecoder,
 )
+from eva.vision.models.networks.decoders.segmentation.semantic.swin_unetr import SwinUNETRDecoder
 from eva.vision.models.networks.decoders.segmentation.semantic.with_image import (
     ConvDecoderWithImage,
 )
 
-__all__ = ["ConvDecoder1x1", "ConvDecoderMS", "SingleLinearDecoder", "ConvDecoderWithImage"]
+__all__ = [
+    "ConvDecoder1x1",
+    "ConvDecoderMS",
+    "ConvDecoderWithImage",
+    "SingleLinearDecoder",
+    "SwinUNETRDecoder",
+]
