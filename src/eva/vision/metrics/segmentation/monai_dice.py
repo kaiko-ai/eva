f4ab85ec5040099d15b3b45aef5127101ac6f026
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-04-11 14:00:10 +0200

    add input_format to MonaiDiceScore

--- src/eva/vision/metrics/segmentation/monai_dice.py
+++ src/eva/vision/metrics/segmentation/monai_dice.py
@@ -1,5 +1,7 @@
 """Wrapper for dice score metric from MONAI."""
 
+from typing import Literal
+
 from monai.metrics.meandice import DiceMetric
 from typing_extensions import override
 
@@ -14,6 +16,7 @@ class MonaiDiceScore(wrappers.MonaiMetricWrapper):
         self,
         num_classes: int,
         include_background: bool = True,
+        input_format: Literal["one-hot", "index"] = "index",
         reduction: str = "mean",
         ignore_index: int | None = None,
         **kwargs,
@@ -24,6 +27,8 @@ class MonaiDiceScore(wrappers.MonaiMetricWrapper):
             num_classes: The number of classes in the dataset.
             include_background: Whether to include the background class in the computation.
             reduction: The method to reduce the dice score. Options are `"mean"`, `"sum"`, `"none"`.
+            input_format: Choose between "one-hot" for one-hot encoded tensors or "index"
+                for index tensors.
             ignore_index: Integer specifying a target class to ignore. If given, this class
                 index does not contribute to the returned score.
             kwargs: Additional keyword arguments for instantiating monai's `DiceMetric` class.
@@ -40,11 +45,13 @@ class MonaiDiceScore(wrappers.MonaiMetricWrapper):
         self.reduction = reduction
         self.orig_num_classes = num_classes
         self.ignore_index = ignore_index
+        self.input_format = input_format
 
     @override
     def update(self, preds, target):
-        preds = _utils.index_to_one_hot(preds, num_classes=self.orig_num_classes)
-        target = _utils.index_to_one_hot(target, num_classes=self.orig_num_classes)
+        if self.input_format == "index":
+            preds = _utils.index_to_one_hot(preds, num_classes=self.orig_num_classes)
+            target = _utils.index_to_one_hot(target, num_classes=self.orig_num_classes)
         if self.ignore_index is not None:
             preds, target = _utils.apply_ignore_index(preds, target, self.ignore_index)
         return super().update(preds, target)
