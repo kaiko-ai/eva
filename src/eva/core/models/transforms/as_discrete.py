7698583a1f19677751373ccf5e3284927c34327e
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-04-14 09:35:56 +0200

    fixes in AsDiscrete

--- src/eva/core/models/transforms/as_discrete.py
+++ src/eva/core/models/transforms/as_discrete.py
@@ -1,10 +1,11 @@
 """Defines the AsDiscrete transformation."""
 
 import torch
-from monai.networks.utils import one_hot
 
 
 class AsDiscrete:
+    """Convert the logits tensor to discrete values."""
+
     def __init__(
         self,
         argmax: bool = False,
@@ -28,13 +29,29 @@ class AsDiscrete:
         self._threshold = threshold
 
     def __call__(self, tensor: torch.Tensor) -> torch.Tensor:
+        """Call method for the transformation."""
         if self._argmax:
             tensor = torch.argmax(tensor, dim=1, keepdim=True)
 
         if self._to_onehot is not None:
-            tensor = one_hot(tensor, num_classes=self._to_onehot, dim=1, dtype=torch.long)
+            tensor = _one_hot(tensor, num_classes=self._to_onehot, dim=1, dtype=torch.long)
 
         if self._threshold is not None:
             tensor = tensor >= self._threshold
 
         return tensor
+
+
+def _one_hot(
+    tensor: torch.Tensor, num_classes: int, dtype: torch.dtype = torch.float, dim: int = 1
+) -> torch.Tensor:
+    """Convert input tensor into one-hot format (implementation taken from MONAI)."""
+    shape = list(tensor.shape)
+    if shape[dim] != 1:
+        raise AssertionError(f"Input tensor must have 1 channel at dim {dim}.")
+
+    shape[dim] = num_classes
+    o = torch.zeros(size=shape, dtype=dtype, device=tensor.device)
+    tensor = o.scatter_(dim=dim, index=tensor.long(), value=1)
+
+    return tensor
