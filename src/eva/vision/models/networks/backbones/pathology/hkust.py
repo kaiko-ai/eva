8c0a2b655835ed7522487453563f4208d8ec3dd5
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-04-22 09:45:12 +0200

    use load_state_dict_from_url function to load gpfm model weights

--- src/eva/vision/models/networks/backbones/pathology/hkust.py
+++ src/eva/vision/models/networks/backbones/pathology/hkust.py
@@ -1,62 +1,13 @@
 """Pathology FMs from Hong Kong University of Science and Technology."""
 
-import os
 import re
-from pathlib import Path
 from typing import Tuple
 
-import torch
-from loguru import logger
+import timm
 from torch import nn
-from torchvision.datasets import utils
 
-from eva.vision.data.datasets import structs
-from eva.vision.models import wrappers
+from eva.core.models.wrappers import _utils
 from eva.vision.models.networks.backbones.registry import register_model
-
-
-def _download_gpfm() -> Path:
-    """Download GPFM model to $HOME/.cache and return file location."""
-    filename = "gpfm_github.pth"
-    resource = structs.DownloadResource(
-        filename=filename,
-        url="https://github.com/birkhoffkiki/GPFM/releases/download/ckpt/GPFM.pth",
-        md5="0dc7e345de84f385d09c8c782b4b3236",
-    )
-    root = Path(os.environ["HOME"]) / ".cache"
-    utils.download_url(resource.url, root, resource.filename, resource.md5)
-    return root / filename
-
-
-def _convert_gpfm(checkpoint_path: Path) -> Path:
-    """Convert GPFM raw checkpoint into timm compatible checkpoint and save it to disk."""
-    new_checkpoint_path = Path(checkpoint_path).parent / "gpfm.pth"
-    if new_checkpoint_path.exists():
-        logger.info("Checkpoint already converted to timm.")
-        return new_checkpoint_path
-    logger.info("Converting github checkpoint to timm...")
-    gpfm_github_state_dict = torch.load(checkpoint_path, weights_only=True)["teacher"]
-    gpfm_github_state_dict = {
-        re.sub(r"blocks\.\d+\.(\d+)", r"blocks.\1", key.replace("backbone.", "")): value
-        for key, value in gpfm_github_state_dict.items()
-    }
-    remove_keys = ["mask_token"] + [
-        key for key in gpfm_github_state_dict.keys() if "dino_head" in key
-    ]
-    for key in remove_keys:
-        gpfm_github_state_dict.pop(key)
-    torch.save(gpfm_github_state_dict, new_checkpoint_path)
-    logger.info(f"Converted checkpoint saved at {str(new_checkpoint_path)}.")
-    return new_checkpoint_path
-
-
-def process_gpfm() -> str:
-    """Download and convert GPFM checkpoint from Github."""
-    # Step 1: download from Github
-    gpfm_github_checkpoint_path = _download_gpfm()
-    # Step 2: convert the checkpoint to make it compatible with timm
-    gpfm_timm_checkpoint_path = _convert_gpfm(gpfm_github_checkpoint_path)
-    return str(gpfm_timm_checkpoint_path)
 
 
 @register_model("pathology/hkust_gpfm")
@@ -78,17 +29,41 @@ def hkust_gpfm(
     Returns:
         The model instance.
     """
-    timm_kwargs = {
-        "img_size": 224,
-        "patch_size": 14,
-        "init_values": 1e-5,
-        "qkv_bias": True,
-        "dynamic_img_size": dynamic_img_size,
-    }
-    return wrappers.TimmModel(
+    return timm.create_model(
         model_name="vit_large_patch14_dinov2",
-        pretrained=False,
-        checkpoint_path=process_gpfm(),
+        pretrained=True,
+        pretrained_cfg={
+            "state_dict": _load_state_dict(),
+            "num_classes": 0,
+        },
         out_indices=out_indices,
-        model_kwargs=timm_kwargs,
+        features_only=out_indices is not None,
+        **{
+            "img_size": 224,
+            "patch_size": 14,
+            "init_values": 1e-5,
+            "qkv_bias": True,
+            "dynamic_img_size": dynamic_img_size,
+        },
     )
+
+
+def _load_state_dict() -> dict:
+    """Loads the state dict with model weights from github."""
+    state_dict = _utils.load_state_dict_from_url(
+        url="https://github.com/birkhoffkiki/GPFM/releases/download/ckpt/GPFM.pth",
+        md5="0dc7e345de84f385d09c8c782b4b3236",
+    )
+    return _convert_state_dict(state_dict["teacher"])
+
+
+def _convert_state_dict(state_dict: dict) -> dict:
+    """Rename state dict keys to match timm's format."""
+    state_dict = {
+        re.sub(r"blocks\.\d+\.(\d+)", r"blocks.\1", key.replace("backbone.", "")): value
+        for key, value in state_dict.items()
+    }
+    remove_keys = ["mask_token"] + [key for key in state_dict.keys() if "dino_head" in key]
+    for key in remove_keys:
+        state_dict.pop(key)
+    return state_dict
