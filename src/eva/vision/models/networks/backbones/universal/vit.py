89faaf9f032b9a55eb66e922020902c94bb1a3fa
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-07-04 14:05:37 +0200

    add 2d configs

--- src/eva/vision/models/networks/backbones/universal/vit.py
+++ src/eva/vision/models/networks/backbones/universal/vit.py
@@ -77,3 +77,28 @@ def vit_small_patch16_224_dino_1chan(
         out_indices=out_indices,
         dynamic_img_size=dynamic_img_size,
     )
+
+
+@register_model("universal/vit_base_patch16_224_dino_1chan")
+def vit_base_patch16_224_dino_1chan(
+    dynamic_img_size: bool = True, out_indices: int | Tuple[int, ...] | None = None
+) -> nn.Module:
+    """Initializes a ViTB-16 baseline model pretrained w/ DINO for single-channel images.
+
+    Args:
+        dynamic_img_size: Support different input image sizes by allowing to change
+            the grid size (interpolate abs and/or ROPE pos) in the forward pass.
+        out_indices: Whether and which multi-level patch embeddings to return.
+
+    Returns:
+        The torch ViTB-16 based foundation model.
+    """
+    return timm.create_model(
+        model_name="vit_base_patch16_224.dino",
+        in_chans=1,
+        num_classes=0,
+        pretrained=True,
+        features_only=out_indices is not None,
+        out_indices=out_indices,
+        dynamic_img_size=dynamic_img_size,
+    )
