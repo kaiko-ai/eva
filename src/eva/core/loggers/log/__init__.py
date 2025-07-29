5f31573bcdd5c20bc5cecbc1f3b13ecfed09e1dd
Nicolas Känzig <36882833+nkaenzig@users.noreply.github.com>
2025-07-29 16:42:42 +0200

    Add `log_table` to `eva.core.loggers` (#860)

--- src/eva/core/loggers/log/__init__.py
+++ src/eva/core/loggers/log/__init__.py
@@ -2,5 +2,6 @@
 
 from eva.core.loggers.log.image import log_image
 from eva.core.loggers.log.parameters import log_parameters
+from eva.core.loggers.log.table import log_table
 
-__all__ = ["log_image", "log_parameters"]
+__all__ = ["log_image", "log_parameters", "log_table"]
