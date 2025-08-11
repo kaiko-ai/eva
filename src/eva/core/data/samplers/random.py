6953a0804168ca77030c987b62c54650393948bd
Max Emanuel Feucht <72391087+MaxFeucht@users.noreply.github.com>
2025-08-11 13:58:34 +0200

    Add `seed` argument to RandomSampler (#866)
    * Add `seed` argument to RandomSampler
    
    * Remove generator as init arg
    
    * lint
    
    * docstring

--- src/eva/core/data/samplers/random.py
+++ src/eva/core/data/samplers/random.py
@@ -2,6 +2,7 @@
 
 from typing import Optional
 
+import torch
 from torch.utils import data
 from typing_extensions import override
 
@@ -10,30 +11,36 @@ from eva.core.data.samplers.sampler import SamplerWithDataSource
 
 
 class RandomSampler(data.RandomSampler, SamplerWithDataSource[int]):
-    """Samples elements randomly."""
+    """Samples elements randomly from a MapDataset."""
 
     data_source: datasets.MapDataset  # type: ignore
 
     def __init__(
-        self, replacement: bool = False, num_samples: Optional[int] = None, generator=None
+        self,
+        replacement: bool = False,
+        num_samples: Optional[int] = None,
+        seed: Optional[int] = None,
     ) -> None:
-        """Initializes the random sampler.
+        """Initialize the random sampler.
 
         Args:
-            data_source: dataset to sample from
-            replacement: samples are drawn on-demand with replacement if ``True``, default=``False``
-            num_samples: number of samples to draw, default=`len(dataset)`.
-            generator: Generator used in sampling.
+            replacement: Samples are drawn on-demand with replacement if ``True``, default=``False``
+            num_samples: Number of samples to draw, default=``len(dataset)``.
+            seed: Optional seed for the random number generator.
         """
         self.replacement = replacement
         self._num_samples = num_samples
-        self.generator = generator
+        self._generator = None
+
+        if seed is not None:
+            self._generator = torch.Generator()
+            self._generator.manual_seed(seed)
 
     @override
     def set_dataset(self, data_source: datasets.MapDataset) -> None:
         super().__init__(
             data_source,
             replacement=self.replacement,
-            num_samples=self.num_samples,
-            generator=self.generator,
+            num_samples=self._num_samples,
+            generator=self._generator,
         )
