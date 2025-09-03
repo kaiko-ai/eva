78bd2857184f03e0675fdf4efdbe0f66bde802d7
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-09-03 14:28:19 +0200

    fix wandb run rename feature for wandb>=0.21.0

--- src/eva/vision/data/datasets/segmentation/lits17.py
+++ src/eva/vision/data/datasets/segmentation/lits17.py
@@ -123,7 +123,7 @@ class LiTS17(VisionDataset[eva_tv_tensors.Volume, tv_tensors.Mask]):
 
     @override
     def validate(self) -> None:
-        requirements.check_dependencies(requirements={"torch": "2.5.1", "torchvision": "0.20.1"})
+        requirements.check_min_versions(requirements={"torch": "2.5.1", "torchvision": "0.20.1"})
 
         def _valid_sample(index: int) -> bool:
             """Indicates if the sample files exist and are reachable."""
