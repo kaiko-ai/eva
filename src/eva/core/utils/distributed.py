28757fb9048fbae20e157cb802bb06385a455da7
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-09-25 11:13:56 +0200

    cleanup

--- src/eva/core/utils/distributed.py
+++ src/eva/core/utils/distributed.py
@@ -1,7 +1,4 @@
-try:
-    import torch.distributed as dist
-except (ImportError, AttributeError):
-    dist = None
+import torch.distributed as dist
 
 
 def is_distributed() -> bool:
@@ -10,9 +7,4 @@ def is_distributed() -> bool:
     Returns:
         bool: True if distributed environment (e.g. multiple gpu processes).
     """
-    return (
-        dist is not None
-        and dist.is_available()
-        and dist.is_initialized()
-        and dist.get_world_size() > 1
-    )
+    return dist.is_available() and dist.is_initialized() and dist.get_world_size() > 1
