a239a99bfc81d883aa5847e0e902b0a8ae2eea8d
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-10-10 15:43:12 +0200

    move FreeFormQuestionPromptTemplate to raw subfolder

--- src/eva/language/prompts/templates/__init__.py
+++ src/eva/language/prompts/templates/__init__.py
@@ -1,7 +1,7 @@
 """Prompt templating API."""
 
 from eva.language.prompts.templates.base import PromptTemplate
-from eva.language.prompts.templates.free_form import FreeFormQuestionPromptTemplate
 from eva.language.prompts.templates.json import JsonMultipleChoicePromptTemplate
+from eva.language.prompts.templates.raw.free_form import FreeFormQuestionPromptTemplate
 
 __all__ = ["PromptTemplate", "JsonMultipleChoicePromptTemplate", "FreeFormQuestionPromptTemplate"]
