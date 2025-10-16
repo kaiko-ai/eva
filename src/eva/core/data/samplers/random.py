3f68827b4fa198fa763902f10fe4859cc703f61b
Max Emanuel Feucht <72391087+MaxFeucht@users.noreply.github.com>
2025-10-16 19:19:32 +0200

    Add `StratifiedRandomSampler` to class samplers (#916)
    * Abstract class samplers and add stratified random sampler
    
    * `num_samples = None` option for stratified Sampler
    
    * Lint
    
    * Add `sample_ratio` arg to both random samplers
    
    * Add tests for both samplers
    
    * rename `class_indices` to `_class_to_indices` and `idx` to `index`
    
    * `class_idx` to `class_id`
    
    * check

--- src/eva/core/data/samplers/random.py
+++ src/eva/core/data/samplers/random.py
@@ -19,6 +19,7 @@ class RandomSampler(data.RandomSampler, SamplerWithDataSource[int]):
         self,
         replacement: bool = False,
         num_samples: Optional[int] = None,
+        sample_ratio: Optional[float] = None,
         seed: Optional[int] = None,
         reset_generator: bool = True,
     ) -> None:
@@ -27,6 +28,8 @@ class RandomSampler(data.RandomSampler, SamplerWithDataSource[int]):
         Args:
             replacement: Samples are drawn on-demand with replacement if ``True``, default=``False``
             num_samples: Number of samples to draw, default=``len(dataset)``.
+            sample_ratio: Alternative to num_samples, specifies the dataset fraction to sample.
+                If both num_samples and sample_ratio are provided, num_samples takes precedence.
             seed: Optional seed for the random number generator.
             reset_generator: Whether to reset the random number generator
                 when setting the dataset. This ensures that repeated runs that share the same
@@ -34,6 +37,7 @@ class RandomSampler(data.RandomSampler, SamplerWithDataSource[int]):
         """
         self.replacement = replacement
         self._num_samples = num_samples
+        self._sample_ratio = sample_ratio
         self._seed = seed
         self._reset_generator = reset_generator
         self._generator = None
@@ -45,10 +49,17 @@ class RandomSampler(data.RandomSampler, SamplerWithDataSource[int]):
         if self._reset_generator:
             self._set_generator()
 
+        num_samples = self._num_samples
+        if num_samples is None and self._sample_ratio is not None:
+            dataset_size = len(data_source)
+            num_samples = int(round(dataset_size * self._sample_ratio))
+        if num_samples is None and self._sample_ratio is None:
+            num_samples = len(data_source)
+
         super().__init__(
             data_source,
             replacement=self.replacement,
-            num_samples=self._num_samples,
+            num_samples=num_samples,
             generator=self._generator,
         )
 
