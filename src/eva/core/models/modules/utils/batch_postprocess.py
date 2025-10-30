0a97a60a7552fa95b9b81a00d838c3376d2f1d74
MaxFeucht <max.emanuel.feucht@gmail.com>
2025-10-30 13:49:59 +0100

    Adapt `apply_transforms` to accomodate factories in postprocessing

--- src/eva/core/models/modules/utils/batch_postprocess.py
+++ src/eva/core/models/modules/utils/batch_postprocess.py
@@ -56,7 +56,12 @@ def _apply_transforms(tensor: torch.Tensor, transforms: List[Transform]) -> torc
     Returns:
         The processed tensor.
     """
-    return functools.reduce(lambda tensor, transform: transform(tensor), transforms, tensor)
+    def apply_transform(tensor: torch.Tensor, transform: Transform) -> torch.Tensor:
+        if isinstance(transform, type) or isinstance(transform, functools.partial):
+            transform = transform()
+        return transform(tensor)
+
+    return functools.reduce(apply_transform, transforms, tensor)
 
 
 def _parse_callable_inputs(inputs: List[Callable | Dict[str, Any]]) -> List[Callable]:
