710d9d40e085498d6e18896342c264bd0bf7fdeb
MaxFeucht <max.emanuel.feucht@gmail.com>
2025-10-30 15:51:07 +0100

    Add freeform extraction tests

--- src/eva/language/models/postprocess/__init__.py
+++ src/eva/language/models/postprocess/__init__.py
@@ -1,8 +1,8 @@
 """Model postprocessing transforms."""
 
-from eva.language.models.postprocess.extract_answer_from_json import ExtractDiscreteAnswerFromJson
-from eva.language.models.postprocess.extract_answer_from_raw import ExtractDiscreteAnswerFromRaw
-from eva.language.models.postprocess.extract_answer_from_xml import ExtractDiscreteAnswerFromXml
+from eva.language.models.postprocess.extract_answer.json import ExtractDiscreteAnswerFromJson
+from eva.language.models.postprocess.extract_answer.raw import ExtractDiscreteAnswerFromRaw
+from eva.language.models.postprocess.extract_answer.xml import ExtractDiscreteAnswerFromXml
 from eva.language.models.postprocess.str_to_int_tensor import CastStrToIntTensor
 
 __all__ = [
---
+++ tests/eva/language/models/postprocess/extract_answer/freeform/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for freeform answer extraction classes."""
