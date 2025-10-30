88018aad12e99634ee20ec549a43f9320304e871
MaxFeucht <max.emanuel.feucht@gmail.com>
2025-10-30 14:18:01 +0100

    Use factory in QuiltVQA dataset

--- src/eva/multimodal/data/datasets/free_form/quilt_vqa.py
+++ src/eva/multimodal/data/datasets/free_form/quilt_vqa.py
@@ -28,7 +28,9 @@ class QuiltVQA(TextImageDataset[str]):
     _license: str = "CC-BY-NC-ND-3.0 (https://creativecommons.org/licenses/by-nc-nd/3.0/ch/deed.de)"
     """Dataset license."""
 
-    _default_prompt_template: templates.PromptTemplate = templates.FreeFormQuestionPromptTemplate()
+    _default_prompt_template: templates.PromptTemplate = templates.FreeFormQuestionPromptTemplate(
+        answer_format="raw"
+    )
     """Default prompt template for formatting questions and context."""
 
     _default_render_kwargs: Dict[str, Any] = {
