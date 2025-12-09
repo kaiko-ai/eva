e390400acd475ccd1c3672af1b5e11e744ba58d7
Nicolas Kaenzig <nkaenzig@gmail.com>
2025-12-09 10:38:55 +0100

    add llama 3.2 model

--- src/eva/language/models/networks/__init__.py
+++ src/eva/language/models/networks/__init__.py
@@ -6,6 +6,7 @@ from eva.language.models.networks.api import (
     Claude37Sonnet20250219,
     Gemini25FlashLite,
 )
+from eva.language.models.networks.meta import Lllama3_2_3BInstruct
 from eva.language.models.networks.microsoft import Phi3Mini4KInstruct
 from eva.language.models.networks.registry import model_registry
 
@@ -15,5 +16,6 @@ __all__ = [
     "Gemini25FlashLite",
     "Qwen205BInstruct",
     "Phi3Mini4KInstruct",
+    "Lllama3_2_3BInstruct",
     "model_registry",
 ]
