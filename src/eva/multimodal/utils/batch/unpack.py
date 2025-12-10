5691bf89e75e26ee5f13d8b6ef21a9778e338225
MaxFeucht <max.emanuel.feucht@gmail.com>
2025-12-10 17:58:16 +0100

    Improve batch typing

--- src/eva/multimodal/utils/batch/unpack.py
+++ src/eva/multimodal/utils/batch/unpack.py
@@ -1,10 +1,22 @@
 """Unpack batch utility function."""
 
+from typing import Any, Dict, List, Tuple
+
+from torchvision import tv_tensors
+
+from eva.language.data.messages import MessageSeries
 from eva.language.models.typings import TextBatch
 from eva.multimodal.models.typings import TextImageBatch
 
 
-def unpack_batch(batch: TextImageBatch | TextBatch) -> tuple:
+def unpack_batch(
+    batch: TextImageBatch | TextBatch,
+) -> Tuple[
+    List[MessageSeries],
+    List[tv_tensors.Image] | None,
+    Any,
+    Dict[str, Any] | None,
+]:
     """Unpacks a TextImageBatch or TextBatch into its components."""
     if isinstance(batch, TextImageBatch):
         return batch.text, batch.images, batch.target, batch.metadata
