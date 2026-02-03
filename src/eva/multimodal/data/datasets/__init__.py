e24ea696961da9f90feff72728d3e390d2a3a4cd
Jonas Erb <jonas.erb@kaiko.ai>
2026-02-03 17:33:36 +0100

    fixing linting errors

--- src/eva/multimodal/data/datasets/__init__.py
+++ src/eva/multimodal/data/datasets/__init__.py
@@ -1,7 +1,11 @@
 """Multimodal datasets API."""
 
 from eva.multimodal.data.datasets.free_form import QuiltVQA
-from eva.multimodal.data.datasets.multiple_choice import PatchCamelyon, PathMMUAtlas, PathMMUEduContent
+from eva.multimodal.data.datasets.multiple_choice import (
+    PatchCamelyon,
+    PathMMUAtlas,
+    PathMMUEduContent,
+)
 from eva.multimodal.data.datasets.text_image import TextImageDataset
 
 __all__ = ["TextImageDataset", "PatchCamelyon", "PathMMUAtlas", "PathMMUEduContent", "QuiltVQA"]
