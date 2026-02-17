9b09117f89da8b34f386e3ef6738b3b44adf2390
ioangatop <johngatop@gmail.com>
2026-02-17 17:07:21 +0100

    [Radiology] Add SuPreM and Universal radiology backbones. (#993)

--- src/eva/vision/models/networks/backbones/radiology/__init__.py
+++ src/eva/vision/models/networks/backbones/radiology/__init__.py
@@ -1,11 +1,18 @@
 """Vision Radiology Model Backbones API."""
 
+from eva.vision.models.networks.backbones.radiology.segresnet import SegResNetEncoder
+from eva.vision.models.networks.backbones.radiology.suprem import SuPreMSegResNet, SuPreMSwinUNETR
 from eva.vision.models.networks.backbones.radiology.swin_unetr import SwinUNETREncoder
+from eva.vision.models.networks.backbones.radiology.universal import UniversalModelSwinUNETR
 from eva.vision.models.networks.backbones.radiology.voco import VoCoB, VoCoH, VoCoL
 
 __all__ = [
-    "VoCoB",
-    "VoCoL",
-    "VoCoH",
+    "SegResNetEncoder",
+    "SuPreMSegResNet",
+    "SuPreMSwinUNETR",
     "SwinUNETREncoder",
+    "UniversalModelSwinUNETR",
+    "VoCoB",
+    "VoCoH",
+    "VoCoL",
 ]
